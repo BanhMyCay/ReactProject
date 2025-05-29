fb4ad8e93e8d15a89cbbf3c63b424e66394290b6
BanhMyCay <chien.tm090598@gmail.com>
2025-05-29 23:37:35 +0700

    MyStore03

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,135 @@
+/**
+ * @title     workouts collection define
+ * @brief     Define structure for workouts collection on Database
+ * @filename  workoutModels.js
+ ----------------------------------------------------------------------------- 
+ * @author    BanhMyCay
+ * @nation    VietNam
+ * @date      29/05/2025
+ */
+
+
+
+/** -------------------------------------------------------------------------- 
+  @NOTE ----------------------------------------------------------------------
+--------------------------------------------------------------------------- */
+
+
+
+/** -------------------------------------------------------------------------- 
+  @IMPORT --------------------------------------------------------------------
+--------------------------------------------------------------------------- */
+/** Component mongoose */
+const mongoose = require('mongoose')                  // component liên kết với tập thư viện dùng để kết nối và làm việc với MongoDB
+
+
+
+/** -------------------------------------------------------------------------- 
+  @COMPONENT -----------------------------------------------------------------
+--------------------------------------------------------------------------- */
+/** Constructor là một hàm đặc biệt được sử dụng để tạo và khởi tạo đối tượng (object) khi sử dụng từ khóa new */
+/** Component gắn với constructor Schema từ mongoose, dùng để định nghĩa cấu trúc dữ liệu cho các documents trong MongoDB */
+const Schema = mongoose.Schema
+
+/** Định nghĩa cấu trúc dữ liệu cho user model dùng trong (assignedUsersList hoặc createdBy) để lưu trên database 
+ * @Arg1  Trường dữ liệu tự tạo
+ *        @displayName  - tên user (kiểu chuỗi, bắt buộc)
+ *        @id           - id của user (kiểu chuỗi, bắt buộc)
+ *        @photoURL     - nguồn Avatar (kiểu chuỗi, mặc định null).
+ * @Arg2  '_id: false'  - tắt tính năng tự động tạo id
+ */
+const userSchema = new Schema({
+  displayName: { 
+    type: String, 
+    required: true 
+  },
+  id: { 
+    type: String, 
+    required: true 
+  },
+  photoURL: { 
+    type: String, 
+    default: null 
+  }
+}, { _id: false })
+
+/** Định nghĩa cấu trúc dữ liệu cho mỗi comment
+ * @Arg1  Trường dữ liệu tự tạo
+ *        @content      - nội dung (kiểu chuỗi, bắt buộc)
+ *        @createdAt    - ngày comment (kiểu ngày tháng, bắt buộc)
+ *        @displayName  - tên người comment (kiểu chuỗi, bắt buộc)
+ *        @id           - id người comment (kiểu chuỗi, bắt buộc)
+ *        @photoURL     - nguồn Avatar người comment (kiểu chuỗi, mặc định null).
+ * @Arg2  '_id: false'  - tắt tính năng tự động tạo id
+ */
+const commentSchema = new Schema({
+  content: { 
+    type: String, 
+    required: true 
+  },
+  createdAt: { 
+    type: Date, 
+    required: true 
+  },
+  displayName: { 
+    type: String, 
+    required: true 
+  },
+  id: {
+    type: Number, 
+    required: true 
+  },
+  photoURL: { 
+    type: String, 
+    default: null 
+  }
+}, { _id: false })
+
+/** Định nghĩa cấu trúc dữ liệu chính cho workout
+ * @Arg1  Trường dữ liệu tự tạo
+ *        @name               - tên workout (kiểu chuỗi, bắt buộc)
+ *        @details            - mô tả workout (kiểu chuỗi, bắt buộc)
+ *        @category           - phân loại workout (kiểu chuỗi, bắt buộc)
+ *        @dueDate            - hạn workout (kiểu ngày giời, bắt buộc)
+ *        @createdAt          - ngày tạo (kiểu ngày giời, mặc định thời điểm hiện tại)
+ *        @createdBy          - người tạo (kiểu 'userSchema', bắt buộc)
+ *        @assignedUsersList  - danh sach người được phân công (kiểu 'userSchema')
+ *        @comments           - các bình luận (kiểu 'commentSchema')
+ * @Arg2  '_id: false'  - tắt tính năng tự động tạo id
+ */
+const workoutSchema = new Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },                    
+  details: { 
+    type: String, 
+    required: true 
+  },   
+  category: { 
+    type: String, 
+    required: true 
+  }, 
+  dueDate: { 
+    type: Date, 
+    required: true 
+  },          
+  createdAt: { 
+    type: Date, 
+    default: Date.now
+  },               
+  createdBy: { 
+    type: userSchema, 
+    required: true 
+  }, 
+  assignedUsersList: { 
+    type: [userSchema], 
+    default: [] 
+  },
+  comments: { type: [commentSchema], default: [] }            // Bình luận
+})
+
+/** Tạo model có tên là 'Workout'. Mongoose sẽ tự map model 'workoutSchema' này sang collection workouts (dạng số nhiều, chữ thường).
+ *  'module.exports' cho phép import model này ở file khác bằng require('./models/Workout') 
+ */
+module.exports = mongoose.model('Workout', workoutSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 /**
- * @title     Express App setup file
- * @brief     File setup Express App
+ * @title     Server setup file
+ * @brief     File setup Server for frontend and backend
  * @filename  server.js
  ----------------------------------------------------------------------------- 
  * @author    BanhMyCay
@@ -11,25 +11,90 @@
 
 
 /** -------------------------------------------------------------------------- 
+  @NOTE ----------------------------------------------------------------------
+--------------------------------------------------------------------------- */
+// npm install express (thư viện express, framework web hỗ trợ xây dựng API giao tiếp giữa browser và database)
+// npm install -g nodemon (package để thực hiện update server khi thay đổi file code - cho dev - chỉ tải 1 lần)
+// npm install dotenv (package dotenv, sử dụng file .env)
+// nodemon server.js (bắt đầu chạy server)
+// npm install mongoose (mongoose là một thư viện (ODM) dùng trong Node.js để làm việc với MongoDB)
+
+
+/** -------------------------------------------------------------------------- 
   @IMPORT --------------------------------------------------------------------
 --------------------------------------------------------------------------- */
-/** Express package */
-const express = require('express')
+/** Express modules API */
+const express = require('express')                    // component liên kết với return của express module
 
+/** Component router app */
+const workoutRoutes = require('./routes/workouts')    // "mini-app" chuyên xử lý các route liên quan đến một phần của ứng dụng
 
+/** Component mongoose */
+const mongoose = require('mongoose')                  // component liên kết với tập thư viện dùng để kết nối và làm việc với MongoDB
+
+/** Dotenv modules */
+require('dotenv').config()                            // tự động tải các biến môi trường từ một file .env vào process.env
 
 
 
 /** -------------------------------------------------------------------------- 
   @COMPONENT -----------------------------------------------------------------
 --------------------------------------------------------------------------- */
-/** Express app component */
+/** Express app component, khởi tạo ứng dụng Express */
 const exapp = express()
 
-/** Lắng nghe requests trên cổng '4000'  */
-exapp.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
+
+
+/** -------------------------------------------------------------------------- 
+  @MIDDLEWARE_FUNCTIONS ------------------------------------------------------
+--------------------------------------------------------------------------- */
+/** Middleware - là các hàm chạy trước khi request được xử lý bởi route chính */
+
+
+/** Tự động phân tích (parse) body của request nếu nội dung là JSON
+ *  Nếu không có dòng này, req.body sẽ là undefined
+ */
+exapp.use(express.json())
+
+/** Tự động tiền xử lý các request trước khi xử lý trong app
+ * @Arg1  req   - request từ route
+ * @Arg2  res   - respone lên route
+ * @Arg3  next  - Gọi route tiếp theo
+ */
+exapp.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
 })
+
+
+
+/** -------------------------------------------------------------------------- 
+  @FUNCTIONS -----------------------------------------------------------------
+--------------------------------------------------------------------------- */
+/** Route xử lý các request đến địa chỉ '/api/workouts' 
+ * @Arg1  '/api/workouts' - đường dẫn gốc của workouts
+ * @Arg2  "mini-app" chuyên xử lý các route
+ */       
+exapp.use('/api/workouts', workoutRoutes)
+
+/** Gọi hàm liên kết với db của mongoDB
+ * @Arg1  'process.env.MONGO_URI' - biến môi trường chứa địa chỉ URI kết nối đến MongoDB
+ */
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {       // hàm xử lý sau khi kết nối thành công
+    console.log('connected to database')
+
+    /** Khởi động server, lắng nghe các request trên cổng được chỉ định
+     * @Arg1  công chỉ định (lấy từ biến môi trường 'PORT')
+     * @Arg2  hàm xử lý sau khi khởi động
+     */
+    exapp.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {   // hàm xử lý khi kết nối có lỗi xảy ra
+    console.log(err)  // log lỗi
+  }) 
 
 
  
